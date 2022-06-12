3527d4763ed0a98be8aba8c7a4a77c81ee2fb0c3
Pablo <pablobormeg@gmail.com>
2022-06-12 13:13:54 +0200

    Prueba clave 3

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
    # end
   #end
   
-  get '' => 'sesion#index'
+  get '' => 'sesion#login'
   # post '', to: 'sesion#create'
   #post '/login' => 'sesion#create'
   post '/login' => 'sesion#login'
